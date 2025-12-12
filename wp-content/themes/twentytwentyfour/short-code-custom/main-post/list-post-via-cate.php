71efb7f5312b16529a30945fcd3102f690170b6a
giapham <giapham121994@gmail.com>
2025-12-12 13:26:09 +0700

    sada

--- wp-content/themes/twentytwentyfour/short-code-custom/main-post/list-post-via-cate.php
+++ wp-content/themes/twentytwentyfour/short-code-custom/main-post/list-post-via-cate.php
@@ -193,7 +193,7 @@ function category_news_styles() {
     }
 
     .side-thumb img {
-      width: 90px;
+      width: 100%;
       height: 70px;
       object-fit: cover;
       border-radius: 4px;
